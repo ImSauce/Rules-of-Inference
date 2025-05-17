46107567caa19563abb7e982863926a5feefa29a
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-05-17 20:34:55 +0800

    1

--- Modus Tollens.py
+++ Modus Tollens.py
@@ -6,5 +6,26 @@ q = True  # "The ground is wet"
 
 
 #!---------------------- [ METHODS ] --------------------#
+# p -> q
 def if_p_then_q(p,q):
     return (not p) or q
+
+
+def modus_tollens(if_p_then_q, q):
+    if if_p_then_q and not q:
+        return True
+    else:
+        return False
+
+
+#?------------ public static void main {} ---------------#
+p_implies_q = if_p_then_q(p, q)
+
+# Apply modus ponens
+result = modus_tollens(p_implies_q, q)
+
+print(" ")
+print("--------------- [ OUTPUT] ---------------")
+print(f"P is {p}, Q is {q}")
+print(f"P → Q is {p_implies_q}")
+print(f"Modus Tollens conclusion: not P is {result}")
